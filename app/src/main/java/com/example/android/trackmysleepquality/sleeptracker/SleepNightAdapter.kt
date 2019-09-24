fac3f66fa78dc1ffe31667b252bcd5a15a3b59e5
Michael Malak <michael.k.malak@gmail.com>
2019-09-24 07:07:38 +0200

    Recycling ViewHolders

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
@@ -33,6 +34,13 @@ class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
     override fun getItemCount() = data.size
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
         var item = data[position]
+        if (item.sleepQuality <= 1){
+            holder.textView.setTextColor(Color.RED)
+        }
+        else{
+            //If we didn't reset, RecycleView will recycle the color
+            holder.textView.setTextColor(Color.BLACK)
+        }
         holder.textView.text = item.sleepQuality.toString()
     }
 
